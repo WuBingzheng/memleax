b343eea8005f2a5318cdb02a0f2841944f0812da
WuBingzheng <wubingzheng@gmail.com>
2016-05-16 14:00:54 +0800

    rm hash.c

--- hash.h
+++ hash.h
@@ -2,11 +2,44 @@
 #define _MLD_HASH_
 
 #include "list.h"
+#include <stdint.h>
 
 #define HASH_SIZE 29989
 
-void hash_add(struct hlist_head *hash, struct hlist_node *node, int len);
-void hash_delete(struct hlist_node *node);
-struct hlist_node *hash_search(struct hlist_head *hash, void *vkey, int len);
+static inline uint32_t hash_slot(void *key, int len)
+{
+	uint32_t *p = key;
+	uint32_t n = 0;
+	int i;
+	for (i = 0; i < len / sizeof(uint32_t); i++) {
+		n ^= p[i];
+	}
+	return n % HASH_SIZE;
+}
+
+static inline void hash_add(struct hlist_head *hash, struct hlist_node *node, int len)
+{
+	void *key = node + 1;
+	hlist_add_head(node, &hash[hash_slot(key, len)]);
+}
+
+static inline void hash_delete(struct hlist_node *node)
+{
+	hlist_del(node);
+}
+
+static inline struct hlist_node *hash_search(
+		struct hlist_head *hash, void *vkey, int len)
+{
+	uint32_t n = hash_slot(vkey, len);
+
+	struct hlist_node *p;
+	hlist_for_each(p, &hash[n]) {
+		if (memcmp(p + 1, vkey, len) == 0) {
+			return p;
+		}
+	}
+	return NULL;
+}
 
 #endif
